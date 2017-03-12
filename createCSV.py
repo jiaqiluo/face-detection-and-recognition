4e0ff60af5bfe2240c35b9020efdb5385bb6597c
jiaqiluo <jiaqiluo123+GitHub@gmail.com>
2017-03-11 21:21:38 -0800

    New Feature: save output to new file

--- createCSV.py
+++ createCSV.py
@@ -6,8 +6,6 @@ import os.path
 
 # This is a tiny script to help you creating a CSV file from a face
 # database with a similar hierarchie:
-#
-#  philipp@mango:~/facerec/data/at$ tree
 #  .
 #  |-- README
 #  |-- s1
@@ -27,18 +25,27 @@ import os.path
 
 if __name__ == "__main__":
 
-  if len(sys.argv) != 2:
-    print "usage: create_csv <base_path>"
+  if len(sys.argv) != 3:
+    print "usage: create_csv <base_path> <filename.csv>"
     sys.exit(1)
+  if (".csv" not in sys.argv[2]):
+      print "error: output needs to be saved as .csv file"
+      print "Retry: create_csv <base_path> <filename.csv>"
+      sys.exit(1)
 
   BASE_PATH = sys.argv[1]
   SEPARATOR = ";"
 
   label = 0
+  temp = ""
   for dirname, dirnames, filenames in os.walk(BASE_PATH):
     for subdirname in dirnames:
       subject_path = os.path.join(dirname, subdirname)
       for filename in os.listdir(subject_path):
         abs_path = "%s/%s" % (subject_path, filename)
-        print "%s%s%d" % (abs_path, SEPARATOR, label)
+        temp +=  "%s%s%d\n" % (abs_path, SEPARATOR, label)
+        # print "%s%s%d" % (abs_path, SEPARATOR, label)
       label = label + 1
+  f = open(sys.argv[2], 'w')
+  f.write(temp)
+  f.close()
